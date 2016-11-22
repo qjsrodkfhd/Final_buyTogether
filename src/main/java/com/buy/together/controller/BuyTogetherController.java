e001561ee96ef29cabf001758afdf989540fab88
jbc91 <qjsrodkfhd@gmail.com>
2016-11-22 17:36:59 +0900

    Controller
    controller -> RestController

--- src/main/java/com/buy/together/controller/BuyTogetherController.java
+++ src/main/java/com/buy/together/controller/BuyTogetherController.java
@@ -4,12 +4,12 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.buy.together.service.BuyTogetherService;
 
-@Controller
+@RestController
 @RequestMapping("")
 public class BuyTogetherController {
 
